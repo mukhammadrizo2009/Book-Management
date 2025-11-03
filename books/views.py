35ae4fd9e4e5189c0950bd31ca00261a650385c5
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-11-03 15:24:58 +0500

    .

--- books/views.py
+++ books/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest , HttpResponse
 # Create your views here.
+def home_view(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Home Page")
